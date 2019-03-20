8b5427b28c9479e504942762a8f205c51c78fac7
Ann Cerveny <cervann11@gmail.com>
2019-03-20 16:41:49 -0600

    Added p1 name and guess JS

--- main.js
+++ main.js
@@ -2,37 +2,24 @@
 var minRange = document.querySelector('.input--min-range');
 var maxRange = document.querySelector('.input--max-range');
 var buttonRangeUpdate = document.querySelector('.button--update-range');
-<<<<<<< HEAD
-
-=======
 var minNumber = document.querySelector('.min-number');
 var maxNumber = document.querySelector('.max-number');
->>>>>>> 6badfbe8c43b490043ff11f0954263ad3367cf0b
+
+var p1NameInput  = document.querySelector('.input--p1-name')
+var p2NameInput  = document.querySelector('.input--p2-name')
+var buttonSubmitGuess = document.querySelector('.button--submit-guess');
+var p1NameOutput  = document.querySelector('.output--p1-name')
+var p2NameOutput  = document.querySelector('.output--p2-name')
+
+var p1GuessInput  = document.querySelector('.input--p1-guess')
+var p2GuessInput  = document.querySelector('.input--p2-guess')
+var p1GuessOutput  = document.querySelector('.output--p1-guess')
+var p2GuessOutput  = document.querySelector('.output--p2-guess')
+
 
 buttonRangeUpdate.addEventListener("click", getRandom);
 
 function getRandom() {
-<<<<<<< HEAD
-  var min = minRange.value;
-  var max = maxRange.value;
-  console.log(min);
-  console.log(max);
-  // newInput(minInput, max)
-  var randomNum = Math.floor(Math.random() * (max - min)) + min;
-  console.log(randomNum);
-  return randomNum;
-}
-
-// function newInput() {
-//   var min = minRange.value;
-//   var max = maxRange.value;
-//   var minInput =  document.querySelector('.input--min-range').value;
-//   var maxInput = document.querySelector('.input--max-range').value;
-
-
-//   document.querySelector('.min-number', '.max-number').innerText=input;
-// }
-=======
  var min = Math.ceil(minRange.value);
  var max = Math.floor(maxRange.value);
  console.log(min);
@@ -44,5 +31,26 @@ function getRandom() {
  console.log(randomNum);
  return randomNum;
 }
->>>>>>> 6badfbe8c43b490043ff11f0954263ad3367cf0b
 
+
+buttonSubmitGuess.addEventListener("click", nameHandler);
+
+
+function nameHandler() {
+ var p1 = p1NameInput.value;
+ var p2 = p2NameInput.value;
+ console.log(p1);
+ console.log(p2);
+
+ p1NameOutput.innerText = p1;
+ p2NameOutput.innerText = p2;
+
+ var g1 = p1GuessInput.value;
+ var g2 = p2GuessInput.value;
+ console.log(g1);
+ console.log(g2);
+
+ p1GuessOutput.innerText = g1;
+ p2GuessOutput.innerText = g2;
+
+}
