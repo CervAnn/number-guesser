11c0b2b4b68523343d2217df7c59325d1f96c04f
Nathan <nathan.froeh@gmail.com>
2019-03-20 11:16:07 -0600

    Add css sizing

--- main.js
+++ main.js
@@ -2,20 +2,21 @@
 var minRange = document.querySelector('.input--min-range');
 var maxRange = document.querySelector('.input--max-range');
 var buttonRangeUpdate = document.querySelector('.button--update-range');
+var minNumber = document.querySelector('.min-number');
+var maxNumber = document.querySelector('.max-number');
 
 buttonRangeUpdate.addEventListener("click", getRandom);
 
 function getRandom() {
  var min = Math.ceil(minRange.value);
  var max = Math.floor(maxRange.value);
- //console.log(min);
- //console.log(max);
+ console.log(min);
+ console.log(max);
 
  var randomNum = Math.floor(Math.random() * (max - min)) + min;
+ minNumber.innerText = min;
+ maxNumber.innerText = max;
  console.log(randomNum);
  return randomNum;
 }
-
-
-
 
